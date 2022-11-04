acebc5239946d1f26fd510b5b9a4788dfc906282
steve <284354851@qq.com>
2022-11-04 10:55:40 +0800

    为方便win上调试，由监听socket改成监听端口

--- cmd/umyproxy/main.go
+++ cmd/umyproxy/main.go
@@ -11,20 +11,20 @@ import (
 )
 
 var (
-    appname string = "umyproxy"
-    version string = "0.0.1"
-    showversion bool
-    host string
-    port int
-    socketfile string
-    poolsize int
-    maxlife int
-    waittimeout int
-    debug bool
+	appname     string = "umyproxy"
+	version     string = "0.0.1"
+	showversion bool
+	host        string
+	port        int
+	socketfile  string
+	poolsize    int
+	maxlife     int
+	waittimeout int
+	debug       bool
 )
 
 const (
-    logstr = `
+	logstr = `
    __  ____  ___      ____
   / / / /  |/  /_  __/ __ \_________  _  ____  __
  / / / / /|_/ / / / / /_/ / ___/ __ \| |/_/ / / /
@@ -32,40 +32,41 @@ const (
 \____/_/  /_/\__, /_/   /_/   \____/_/|_|\__, /
             /____/                      /____/
     `
+	LISTEN_ADDRESS = ":13306"
 )
 
 func init() {
-    flag.BoolVar(&showversion, "version", false, "show version")
-    flag.StringVar(&host, "host", "127.0.0.1", "mysql host")
-    flag.IntVar(&port, "port", 3306, "mysql port")
-    flag.StringVar(&socketfile, "socket", "/tmp/"+ appname + ".socket", "socket file path")
-    flag.IntVar(&poolsize, "size", runtime.NumCPU(), "pool size")
-    flag.IntVar(&maxlife, "life", 3600, "mysql connection max life time")
-    flag.IntVar(&waittimeout, "wait", 3000, "wait mysql connection timeout")
-    flag.BoolVar(&debug, "debug", false, "set debug mode")
+	flag.BoolVar(&showversion, "version", false, "show version")
+	flag.StringVar(&host, "host", "127.0.0.1", "mysql host")
+	flag.IntVar(&port, "port", 3306, "mysql port")
+	flag.StringVar(&socketfile, "socket", "/tmp/"+appname+".socket", "socket file path")
+	flag.IntVar(&poolsize, "size", runtime.NumCPU(), "pool size")
+	flag.IntVar(&maxlife, "life", 3600, "mysql connection max life time")
+	flag.IntVar(&waittimeout, "wait", 3000, "wait mysql connection timeout")
+	flag.BoolVar(&debug, "debug", false, "set debug mode")
 }
 
-func main()  {
-    flag.Parse()
+func main() {
+	flag.Parse()
 
-    if showversion {
-        fmt.Println(appname, version)
-        os.Exit(0)
-    }
-    fmt.Println(appname, version)
-    fmt.Println(logstr)
+	if showversion {
+		fmt.Println(appname, version)
+		os.Exit(0)
+	}
+	fmt.Println(appname, version)
+	fmt.Println(logstr)
 
-    option := proxy.PoolOption{
-        Host: host,
-        Port: port,
-        MaxLifetime: time.Second * time.Duration(maxlife),
-        PoolMaxSize: poolsize,
-        WaitTimeout: time.Millisecond * time.Duration(waittimeout),
-    }
+	option := proxy.PoolOption{
+		Host:        host,
+		Port:        port,
+		MaxLifetime: time.Second * time.Duration(maxlife),
+		PoolMaxSize: poolsize,
+		WaitTimeout: time.Millisecond * time.Duration(waittimeout),
+	}
 
-    p := proxy.NewProxy(proxy.NewPool(option), socketfile)
-    if debug {
-        p.SetDebug()
-    }
-    p.Run()
+	p := proxy.NewProxy(proxy.NewPool(option), socketfile, LISTEN_ADDRESS)
+	if debug {
+		p.SetDebug()
+	}
+	p.Run()
 }
--- internal/proxy/proxy.go
+++ internal/proxy/proxy.go
@@ -8,132 +8,136 @@ import (
 )
 
 type (
-    Proxy struct {
-        server net.Listener
-        pool *Pool
-        socketFile string
-        debug bool
-    }
+	Proxy struct {
+		server     net.Listener
+		pool       *Pool
+		socketFile string
+		listenAddr string
+		debug      bool
+	}
 )
 
-func NewProxy(p *Pool, socketfile string) *Proxy {
-    return &Proxy{
-        pool: p,
-        socketFile: socketfile,
-    }
+func NewProxy(p *Pool, socketfile string, listenAddr string) *Proxy {
+	return &Proxy{
+		pool:       p,
+		socketFile: socketfile,
+		listenAddr: listenAddr,
+	}
 }
 
 func (p *Proxy) Run() {
-    p.deleteSocketFile()
+	//修改原有的socket监听，改成监听端口
+	//p.deleteSocketFile()
+	//serv, err := net.Listen("unix", p.socketFile)
 
-    serv, err := net.Listen("unix", p.socketFile)
-    if err != nil {
-        log.Fatalln("Listen err:", err)
-    }
-    p.server = serv
-    p.startPrint()
+	serv, err := net.Listen("tcp", p.listenAddr)
+	if err != nil {
+		log.Fatalln("Listen err:", err)
+	}
+	p.server = serv
+	p.startPrint()
 
-    for {
-        conn, err := p.server.Accept()
-        if err != nil {
-            log.Fatalln("conn err:", err)
-        }
-        p.debugPrintf("accept conn")
+	for {
+		conn, err := p.server.Accept()
+		if err != nil {
+			log.Fatalln("conn err:", err)
+		}
+		p.debugPrintf("accept conn")
 
-        go p.HandleConn(conn)
-    }
+		go p.HandleConn(conn)
+	}
 }
 
 func (p *Proxy) SetDebug() {
-    p.debug = true
-    p.debugPrintf("debug mode")
+	p.debug = true
+	p.debugPrintf("debug mode")
 }
 
 func (p *Proxy) debugPrintf(format string, v ...interface{}) {
-    if p.debug {
-        format = "[DEBUG]" + format + "\n"
-        log.Printf(format, v...)
-    }
+	if p.debug {
+		format = "[DEBUG]" + format + "\n"
+		log.Printf(format, v...)
+	}
 }
 
 func (p *Proxy) startPrint() {
-    log.Println("start server: ", p.socketFile)
-    log.Println("host:", p.pool.option.Host)
-    log.Println("port:", p.pool.option.Port)
-    log.Println("pool_size:", p.pool.option.PoolMaxSize)
-    log.Println("conn_maxlifetime:", p.pool.option.MaxLifetime)
-    log.Println("wait_timeout:", p.pool.option.WaitTimeout)
+	log.Println("start server: ", p.socketFile)
+	log.Println("host:", p.pool.option.Host)
+	log.Println("port:", p.pool.option.Port)
+	log.Println("pool_size:", p.pool.option.PoolMaxSize)
+	log.Println("conn_maxlifetime:", p.pool.option.MaxLifetime)
+	log.Println("wait_timeout:", p.pool.option.WaitTimeout)
 }
 
 func (p *Proxy) HandleConn(conn net.Conn) {
-    client := protocol.NewConn(conn)
-    defer client.Close()
+	client := protocol.NewConn(conn)
+	defer client.Close()
 
-    mysqlServ, err := p.Get()
-    if err != nil {
-        log.Printf("get mysql conn err: %+v \n", err)
-        return
-    }
-    p.debugPrintf("get mysql conn")
-    defer p.Put(mysqlServ)
+	mysqlServ, err := p.Get()
+	if err != nil {
+		log.Printf("get mysql conn err: %+v \n", err)
+		return
+	}
+	p.debugPrintf("get mysql conn")
+	defer p.Put(mysqlServ)
 
-    // 认证
-    if err := mysqlServ.Auth(client); err != nil {
-        log.Printf("mysql auth err: %+v \n", err)
-        return
-    }
-    p.debugPrintf("client auth success")
+	// 认证
+	if err := mysqlServ.Auth(client); err != nil {
+		log.Printf("mysql auth err: %+v \n", err)
+		return
+	}
+	p.debugPrintf("client auth success")
 
-    // 发送命令
-    for {
-        cmdPacket, err := client.ReadPacket()
-        if err != nil {
-            log.Printf("read client cmd err: %+v \n", err)
-            break
-        }
-        p.debugPrintf("read cmd: %+v", cmdPacket)
+	// 发送命令
+	for {
+		cmdPacket, err := client.ReadPacket()
+		if err != nil {
+			log.Printf("read client cmd err: %+v \n", err)
+			break
+		}
+		p.debugPrintf("read cmd: %+v", cmdPacket)
 
-        if protocol.IsQuitPacket(cmdPacket) {
-            p.debugPrintf("quit cmd")
-            break
-        }
+		if protocol.IsQuitPacket(cmdPacket) {
+			p.debugPrintf("quit cmd")
+			break
+		}
 
-        err2 := mysqlServ.WritePacket(cmdPacket)
-        if err2 != nil {
-            log.Printf("write cmd packet to server err: %+v \n", err2)
-            break
-        }
-        p.debugPrintf("write cmd to mysql")
+		err2 := mysqlServ.WritePacket(cmdPacket)
+		if err2 != nil {
+			log.Printf("write cmd packet to server err: %+v \n", err2)
+			break
+		}
+		p.debugPrintf("write cmd to mysql")
 
-        // read response
-        p.debugPrintf("start transport mysql response")
-        err = mysqlServ.TransportCmdResp(client)
-        if err != nil {
-            log.Printf("transport response err: %+v \n", err)
-            break
-        }
-        p.debugPrintf("end transport mysql response")
-    }
+		// read response
+		p.debugPrintf("start transport mysql response")
+		err = mysqlServ.TransportCmdResp(client)
+		if err != nil {
+			log.Printf("transport response err: %+v \n", err)
+			break
+		}
+		p.debugPrintf("end transport mysql response")
+	}
 }
 
 func (p *Proxy) Get() (protocol.Connector, error) {
-    return p.pool.Get()
+	return p.pool.Get()
 }
 
 func (p *Proxy) Put(conn protocol.Connector) error {
-    p.debugPrintf("put conn")
-    return p.pool.Put(conn)
+	p.debugPrintf("put conn")
+	return p.pool.Put(conn)
 }
 
 func (p *Proxy) Close() {
-    p.pool.Close()
-    p.server.Close()
+	p.pool.Close()
+	p.server.Close()
 }
 
 func (p *Proxy) deleteSocketFile() error {
-    _, err := os.Stat(p.socketFile)
-    if err == nil || os.IsExist(err) {
-        return os.Remove(p.socketFile)
-    }
-    return err
+	_, err := os.Stat(p.socketFile)
+	if err == nil || os.IsExist(err) {
+		return os.Remove(p.socketFile)
+	}
+	return err
 }
